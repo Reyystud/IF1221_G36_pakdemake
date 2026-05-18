7d1b72259e0d3d0583a865216aa0335998e431b5
cepsssy <stevenvanako1@gmail.com>
2026-05-19 04:06:11 +0700

    Adding Intro Output Message

---
+++ src/main.pl
@@ -0,0 +1,9 @@
+:- ensure_loaded('declarations.pl').
+
+:- nl,
+   write('================================='), nl,
+   write('  UNI Card Game - Prolog Edition '), nl,
+   write('  IF1221 Logika Komputasional    '), nl,
+   write('================================='), nl,
+   write('Ketik startGame. untuk memulai.  '), nl,
+   write('Ketik lihatCommand. untuk bantuan.'), nl, nl.

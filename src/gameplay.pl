85704fb82f8ef88accae5cf9d1a5bd5503e1f0af
cepsssy <stevenvanako1@gmail.com>
2026-05-31 17:29:08 +0700

    docs: Adding gameplay rules for taking card and recycling deck

--- src/gameplay.pl
+++ src/gameplay.pl
@@ -9,7 +9,6 @@
 :- use_module(declarations).
 :- use_module(utils).
 
-%% kartu_bisa_dimainkan(+Kartu, -Alasan)
 kartu_bisa_dimainkan(kartu(hitam, _), wild_always) :- !.
 kartu_bisa_dimainkan(kartu(W, _), cocok_warna) :-
     declarations:warna_aktif(W), !.
@@ -23,6 +22,60 @@ next_giliran(Berikutnya) :-
     retract(declarations:giliran(Current)),
     assertz(declarations:giliran(Berikutnya)).
 
+mainkan_kartu_index(Pemain, Index, Kartu) :-
+    declarations:tangan(Pemain, Tangan),
+    (   nth1(Index, Tangan, Kartu)
+    ->  (   kartu_bisa_dimainkan(Kartu, _)
+        ->  delete_nth1(Index, Tangan, TanganBaru),
+            retract(declarations:tangan(Pemain, Tangan)),
+            assertz(declarations:tangan(Pemain, TanganBaru)),
+            declarations:discard_pile(Discard),
+            retract(declarations:discard_pile(Discard)),
+            assertz(declarations:discard_pile([Kartu|Discard])),
+            update_warna_aktif(Kartu)
+        ;   format('Kartu tidak valid untuk dimainkan.~n', []), fail
+        )
+    ;   format('Nomor urut kartu tidak valid.~n', []), fail
+    ).
 
+delete_nth1(1, [_|T], T) :- !.
+delete_nth1(N, [H|T], [H|R]) :-
+    N > 1,
+    N1 is N - 1,
+    delete_nth1(N1, T, R).
+
+update_warna_aktif(kartu(hitam, _)) :- !. 
+update_warna_aktif(kartu(W, _)) :-
+    retract(declarations:warna_aktif(_)),
+    assertz(declarations:warna_aktif(W)).
+
+ambil_kartu(Pemain, N, KartuDiambil) :-
+    declarations:draw_pile(Pile),
+    length(Pile, Len),
+    (   Len < N
+    ->  recycle_deck,
+        declarations:draw_pile(Pile2),
+        ambil_n_kartu(N, Pile2, KartuDiambil, SisaPile)
+    ;   ambil_n_kartu(N, Pile, KartuDiambil, SisaPile)
+    ),
+    retract(declarations:draw_pile(_)),
+    assertz(declarations:draw_pile(SisaPile)),
+    declarations:tangan(Pemain, TanganLama),
+    append(TanganLama, KartuDiambil, TanganBaru),
+    retract(declarations:tangan(Pemain, TanganLama)),
+    assertz(declarations:tangan(Pemain, TanganBaru)).
+
+ambil_n_kartu(0, Pile, [], Pile) :- !.
+ambil_n_kartu(N, [K|Rest], [K|Ambil], Sisa) :-
+    N > 0, N1 is N - 1,
+    ambil_n_kartu(N1, Rest, Ambil, Sisa).
+
+recycle_deck :-
+    declarations:discard_pile([Top|Bekas]),
+    utils:shuffle(Bekas, Shuffled),
+    retract(declarations:discard_pile(_)),
+    assertz(declarations:discard_pile([Top])),
+    retract(declarations:draw_pile(_)),
+    assertz(declarations:draw_pile(Shuffled)).
 
 

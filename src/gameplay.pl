264efbdda07b603c6a19b63cc668c99a279a3c62
cepsssy <stevenvanako1@gmail.com>
2026-05-31 18:52:06 +0700

    refactor: Updating the turn mechanics

--- src/gameplay.pl
+++ src/gameplay.pl
@@ -1,15 +1,25 @@
 :- module(gameplay, [
     kartu_bisa_dimainkan/2,
     mainkan_kartu_index/3,
-    ambil_kartu/3,
+    ambil_kartu/4,
     recycle_deck/0,
-    next_giliran/1
+    next_giliran/1,
+    skip_player/0,
+    reverse_direction/0,
+    apply_card_effect/2,
+    process_mimic/2,
+    hitung_poin/2
 ]).
 
 :- use_module(declarations).
 :- use_module(utils).
 
-kartu_bisa_dimainkan(kartu(hitam, _), wild_always) :- !.
+kartu_bisa_dimainkan(kartu(hitam, Jenis), wild_special) :-
+    declarations:discard_pile([Top|_]),
+    (   Top = kartu(hitam, _)
+    ->  format('Kesalahan: Tidak bisa mengeluarkan kartu hitam di atas kartu hitam!~n', []), fail
+    ;   true
+    ), !.
 kartu_bisa_dimainkan(kartu(W, _), cocok_warna) :-
     declarations:warna_aktif(W), !.
 kartu_bisa_dimainkan(kartu(_, J), cocok_jenis) :-
@@ -18,9 +28,25 @@ kartu_bisa_dimainkan(kartu(_, J), cocok_jenis) :-
 next_giliran(Berikutnya) :-
     declarations:giliran(Current),
     declarations:urutan_pemain(Urutan),
-    utils:next_player(Urutan, Current, Berikutnya),
+    declarations:arah(Arah),
+    (   Arah == kanan
+    ->  utils:next_player(Urutan, Current, Berikutnya)
+    ;   utils:prev_player(Urutan, Current, Berikutnya)
+    ),
     retract(declarations:giliran(Current)),
-    assertz(declarations:giliran(Berikutnya)).
+    assertz(declarations:giliran(Berikutnya)),
+    retractall(declarations:status_uni(Berikutnya)).
+
+skip_player :-
+    next_giliran(_).
+
+reverse_direction :-
+    declarations:arah(Arah),
+    retract(declarations:arah(Arah)),
+    (   Arah == kanan
+    ->  assertz(declarations:arah(kiri))
+    ;   assertz(declarations:arah(kanan))
+    ).
 
 mainkan_kartu_index(Pemain, Index, Kartu) :-
     declarations:tangan(Pemain, Tangan),
@@ -32,8 +58,15 @@ mainkan_kartu_index(Pemain, Index, Kartu) :-
             declarations:discard_pile(Discard),
             retract(declarations:discard_pile(Discard)),
             assertz(declarations:discard_pile([Kartu|Discard])),
-            update_warna_aktif(Kartu)
-        ;   format('Kartu tidak valid untuk dimainkan.~n', []), fail
+            update_warna_aktif(Kartu),
+            (   (utils:kartu_aksi(Kartu) ; utils:kartu_hitam(Kartu))
+            ->  retractall(declarations:last_action_card(_)),
+                assertz(declarations:last_action_card(Kartu))
+            ;   true
+            ),
+            apply_card_effect(Kartu, Pemain)
+        ;  
+            fail
         )
     ;   format('Nomor urut kartu tidak valid.~n', []), fail
     ).
@@ -49,7 +82,7 @@ update_warna_aktif(kartu(W, _)) :-
     retract(declarations:warna_aktif(_)),
     assertz(declarations:warna_aktif(W)).
 
-ambil_kartu(Pemain, N, KartuDiambil) :-
+ambil_kartu(Pemain, N, Alasan, KartuDiambil) :-
     declarations:draw_pile(Pile),
     length(Pile, Len),
     (   Len < N
@@ -63,7 +96,8 @@ ambil_kartu(Pemain, N, KartuDiambil) :-
     declarations:tangan(Pemain, TanganLama),
     append(TanganLama, KartuDiambil, TanganBaru),
     retract(declarations:tangan(Pemain, TanganLama)),
-    assertz(declarations:tangan(Pemain, TanganBaru)).
+    assertz(declarations:tangan(Pemain, TanganBaru)),
+    format('~w mengambil ~w kartu karena ~w.~n', [Pemain, N, Alasan]).
 
 ambil_n_kartu(0, Pile, [], Pile) :- !.
 ambil_n_kartu(N, [K|Rest], [K|Ambil], Sisa) :-
@@ -77,5 +111,7 @@ recycle_deck :-
     assertz(declarations:discard_pile([Top])),
     retract(declarations:draw_pile(_)),
     assertz(declarations:draw_pile(Shuffled)).
+
+
 
 

b67bfb14e42e260aad2f60869ada03fe79a04025
cepsssy <stevenvanako1@gmail.com>
2026-05-31 17:40:10 +0700

    docs: Stating rules for startGame. command output

--- src/main.pl
+++ src/main.pl
@@ -1,9 +1,39 @@
-:- ensure_loaded('declarations.pl').
+:- module(main, [
+    startGame/0,
+    mainkanKartu/1,
+    ambilKartu/0,
+    lihatCommand/0,
+    lihatKartu/0,
+    cekInfo/0
+]).
 
-:- nl,
-   write('================================='), nl,
-   write('  UNI Card Game - Prolog Edition '), nl,
-   write('  IF1221 Logika Komputasional    '), nl,
-   write('================================='), nl,
-   write('Ketik startGame. untuk memulai.  '), nl,
-   write('Ketik lihatCommand. untuk bantuan.'), nl, nl.
+:- use_module(declarations).
+:- use_module(init).
+:- use_module(gameplay).
+:- use_module(commands).
+:- use_module(utils).
+
+startGame :-
+    init:reset_game,
+    init:input_jumlah_pemain(N),
+    init:input_nama_pemain(N, ListNama),
+    init:acak_urutan(ListNama, UrutanAcak),
+    utils:format_urutan(UrutanAcak, UrutanTeks),
+    format('~nUrutan pemain: ~w.~n', [UrutanTeks]),
+    
+    init:buat_deck(Deck),
+    init:shuffle_deck(Deck, ShuffledDeck),
+    init:bagikan_kartu(UrutanAcak, ShuffledDeck, SisaDeck),
+    format('~nSetiap pemain mendapatkan 7 kartu acak.~n', []),
+    
+    init:inisiasi_discard(SisaDeck, KartuAwal, DrawPile),
+    utils:format_kartu(KartuAwal, TopTeks),
+    format('~nKartu discard top: ~w.~n', [TopTeks]),
+    
+    utils:get_warna(KartuAwal, WarnaAwal),
+    init:setup_state(UrutanAcak, DrawPile, KartuAwal, WarnaAwal),
+    assertz(declarations:game_started),
+    
+    declarations:giliran(Current),
+    format('~nGiliran ~w.~n', [Current]).
+

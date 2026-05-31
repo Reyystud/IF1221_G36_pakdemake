e16da013bc09e0dcc800e5ab36e497e1ccc57d08
cepsssy <stevenvanako1@gmail.com>
2026-05-31 18:59:26 +0700

    Add: Adding 'tantang', 'uni', and end game system

--- src/main.pl
+++ src/main.pl
@@ -103,6 +103,81 @@ pilihWarna(Warna) :-
 pilihWarna(_) :-
     format('Warna tidak valid! Gunakan merah, kuning, hijau, atau biru.~n', []).
 
+tantang :-
+    \+ declarations:can_challenge(_), !,
+    format('Tidak ada tantangan yang bisa dilakukan saat ini.~n', []).
+tantang :-
+    declarations:giliran(Challenger),
+    declarations:urutan_pemain(Urutan),
+    declarations:arah(Arah),
+    (   Arah == kanan
+    ->  utils:prev_player(Urutan, Challenger, Target)
+    ;   utils:next_player(Urutan, Challenger, Target)
+    ),
+    format('Tantangan dilakukan! Memeriksa kartu ~w...~n', [Target]),
+    declarations:discard_pile([_|SisaDiscard]),
+    SisaDiscard = [PrevTop|_],
+    utils:get_warna(PrevTop, PrevWarna),
+    declarations:tangan(Target, Hand),
+    (   (member(kartu(PrevWarna, _), Hand) ; member(kartu(_, J), Hand), PrevTop = kartu(_, J))
+    ->  format('Tantangan berhasil! ~w memiliki kartu yang cocok.~n', [Target]),
+        gameplay:ambil_kartu(Target, 4, 'tantangan berhasil', _),
+        retractall(declarations:can_challenge(_))
+    ;   format('Tantangan gagal. ~w tidak memiliki kartu lain yang cocok.~n', [Target]),
+        gameplay:ambil_kartu(Challenger, 6, 'tantangan gagal', _),
+        retractall(declarations:can_challenge(_)),
+        gameplay:skip_player
+    ),
+    declarations:giliran(Current),
+    format('~nGiliran ~w.~n', [Current]).
+
+uni(Index) :-
+    declarations:giliran(Pemain),
+    declarations:tangan(Pemain, Tangan),
+    length(Tangan, Len),
+    (   Len == 2
+    ->  assertz(declarations:status_uni(Pemain)),
+        format('~w menyerukan UNI!~n', [Pemain]),
+        mainkanKartu(Index)
+    ;   format('Perintah tidak valid! Anda mendapatkan 1 kartu penalti.~n', []),
+        gameplay:ambil_kartu(Pemain, 1, 'penalti salah seru UNI', _),
+        gameplay:next_giliran(Berikutnya),
+        format('~nGiliran ~w.~n', [Berikutnya])
+    ).
+
+tangkap(Target) :-
+    declarations:giliran(Catcher),
+    declarations:tangan(Target, Tangan),
+    length(Tangan, Len),
+    (   Len == 1, \+ declarations:status_uni(Target)
+    ->  format('~w tertangkap tidak menyerukan UNI!~n', [Target]),
+        gameplay:ambil_kartu(Target, 2, 'tertangkap tidak UNI', _),
+        format('~w mendapatkan 2 kartu penalti.~n', [Target])
+    ;   format('Tuduhan tidak valid! ~w mendapatkan 1 kartu penalti.~n', [Catcher]),
+        gameplay:ambil_kartu(Catcher, 1, 'tuduhan tangkap salah', _)
+    ).
+
+check_tantang_notif(Pemain) :-
+    (   declarations:can_challenge(_)
+    ->  format('~w, Anda dapat menggunakan perintah "tantang." atau "ambilKartu."~n', [Pemain])
+    ;   true
+    ).
+
+end_game(Winner) :-
+    format('~n=================================~n', []),
+    format('Permainan selesai! ~w menghabiskan semua kartunya!~n', [Winner]),
+    format('~nBerikut perhitungan poin sisa kartu:~n', []),
+    declarations:urutan_pemain(Urutan),
+    maplist(print_poin_pemain, Urutan),
+    format('~nSelamat, ~w menjadi pemenang!~n', [Winner]),
+    init:reset_game.
+
+print_poin_pemain(P) :-
+    gameplay:hitung_poin(P, Poin),
+    format('~w: ~w poin~n', [P, Poin]).
+
+lihatCommand :- intro_help.
+
 ambilKartu :-
     \+ declarations:game_started, !, format('Game belum dimulai.~n', []).
 ambilKartu :-

2a1e62f178bd651fbd1982a1aa8debf6d17683f1
cepsssy <stevenvanako1@gmail.com>
2026-05-31 17:20:08 +0700

    docs: Updating the declarations.pl

--- src/declarations.pl
+++ src/declarations.pl
@@ -1,10 +1,17 @@
-:- module(declarations, []).
+:- module(declarations, [
+    game_started/0,
+    urutan_pemain/1,
+    giliran/1,
+    tangan/2,
+    draw_pile/1,
+    discard_pile/1,
+    warna_aktif/1
+]).
+
 :- dynamic game_started/0.        
 :- dynamic urutan_pemain/1.         
 :- dynamic giliran/1.               
-:- dynamic arah/1.                  
 :- dynamic tangan/2.                
 :- dynamic draw_pile/1.             
 :- dynamic discard_pile/1.         
 :- dynamic warna_aktif/1.           
-:- dynamic pending_draw/1.         

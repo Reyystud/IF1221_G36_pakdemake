40d84e4736904e50d6b4e2f21b2f4975862a1201
cepsssy <stevenvanako1@gmail.com>
2026-05-31 20:06:10 +0700

    refactor: editing lihat_command feature and adjusting to tour mode

--- src/commands.pl
+++ src/commands.pl
@@ -6,20 +6,52 @@
 
 :- use_module(declarations).
 :- use_module(utils).
+:- use_module(library(lists)).
 
 lihat_command :-
     format('Aksi utama yang tersedia:~n', []),
-    format('1. ambilKartu~n', []),
-    format('2. mainkanKartu(NomorUrutKartuDiTangan)~n', []),
+    format('1. mainkanKartu(N).    - Memainkan kartu indeks ke-N.~n', []),
+    format('2. ambilKartu.         - Mengambil kartu (sesuai penalti jika ada).~n', []),
+    
+    format('~nAksi tambahan yang tersedia:~n', []),
+    format('1. godsHand.           - Kejadian acak (miracle).~n', []),
+    format('2. sembunyikanKartu(N) - Sembunyikan kartu ke-N.~n', []),
+    format('3. tampilkanKartu.     - Munculkan kartu tersembunyi.~n', []),
+    
+    (   declarations:mode(turnamen)
+    ->  format('11. swapKartu(N, M).   - [BONUS] Tukar kartu dengan teman tim.~n', [])
+    ;   true
+    ),
+    
+    format('1. saveGame.          - Simpan kondisi permainan.~n', []),
+    format('2. loadGame.          - Muat kondisi permainan.~n', []),
+    
     format('~nAksi pendukung yang tersedia:~n', []),
-    format('1. lihatCommand~n', []),
-    format('2. lihatKartu~n', []),
-    format('3. cekInfo~n', []).
+    format('1. lihatCommand.       - Menampilkan bantuan ini.~n', []),
+    format('2. lihatKartu.         - Melihat kartu di tangan.~n', []),
+    format('3. cekInfo.            - Melihat status permainan.~n', []).
 
 lihat_kartu(Pemain) :-
     declarations:tangan(Pemain, Tangan),
     format('Berikut kartu yang anda miliki.~n', []),
-    print_kartu_list(1, Tangan).
+    print_kartu_list(1, Tangan),
+    (   declarations:kartu_tersembunyi(Pemain, K)
+    ->  utils:format_kartu(K, Teks),
+        format('~n[TERSEMBUNYI] ~w~n', [Teks])
+    ;   true
+    ),
+    
+    declarations:mode(Mode),
+    (   Mode == turnamen
+    ->  (   (declarations:tim(1, T1), member(Pemain, T1)) -> Team = T1 
+        ;   (declarations:tim(2, T2), member(Pemain, T2)) -> Team = T2 
+        ),
+        delete(Team, Pemain, [Partner]),
+        declarations:tangan(Partner, PartnerTangan),
+        format('~nBerikut kartu yang teman satu tim anda miliki (~w).~n', [Partner]),
+        print_kartu_list(1, PartnerTangan)
+    ;   true
+    ).
 
 print_kartu_list(_, []) :- !.
 print_kartu_list(I, [K|Ks]) :-
@@ -32,6 +64,15 @@ cek_info :-
     declarations:discard_pile([Top|_]),
     utils:format_kartu(Top, TopTeks),
     format('Kartu discard top: ~w.~n', [TopTeks]),
+    
+    declarations:mode(Mode),
+    (   Mode == turnamen
+    ->  declarations:tim(1, Tim1), declarations:tim(2, Tim2),
+        format('Tim 1 : ~q~n', [Tim1]),
+        format('Tim 2 : ~q~n', [Tim2])
+    ;   true
+    ),
+    
     declarations:urutan_pemain(Urutan),
     utils:format_urutan(Urutan, UrutanTeks),
     format('~nUrutan pemain: ~w.~n', [UrutanTeks]),
